ac934ba92215219b5960eb188bd351f71f3a8a49
Faxriddinf05 <mffahriddin2005@gmail.com>
2026-02-27 16:52:08 +0500

    Finished

--- app/routers/books.py
+++ app/routers/books.py
@@ -21,3 +21,20 @@ def add_books(data: AddBook, db = Depends(get_db)):
     db.add(new_books)
     db.commit()
     return "Added"
+
+@books_router.put('/')
+def edit_books(id_books: int, data: EditBook, db = Depends(get_db)):
+    book_entity = db.execute(select(Book).where(Book.id == id_books)).scalars().first()
+    book_entity.title = data.title
+    book_entity.price = data.price
+    book_entity.author_id = data.author_id
+
+    db.commit()
+    return "Edited"
+
+@books_router.delete('/')
+def remove(id_books: int, db = Depends(get_db)):
+    book_entity = db.execute(select(Book).where(Book.id == id_books)).scalars().first()
+
+    db.delete(book_entity)
+    return "Deleted"
